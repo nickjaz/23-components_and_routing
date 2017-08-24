bd926e71ca408d174d80b1875160ab20b2c1e38f
Nick Asmussen <nick.asmussen1@gmail.com>
2017-08-23 18:27:24 -0700

    builds out components and some functionality

---
+++ src/component/note-create-form/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+class NoteCreateForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      content: '',
+      editing: false,
+      completed: false
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.handleNoteCreate(this.state);
+  }
+
+  render() {
+    return (
+      <form onsubmit={this.handleSubmit}>
+        <input
+          name='title'
+          type='text'
+          value={this.state.title}
+          onChange={this.handleChange}
+        />
+        <input
+          name='content'
+          type='text'
+          value={this.state.content}
+          onChange={this.handleChange}
+        />
+        <button type='submit'>Add</button>
+      </form>
+    )
+  }
+}
+
+export default NoteCreateForm;
---
+++ src/main.js
@@ -0,0 +1,42 @@
+import './style/main.scss';
+import React from 'react';
+import ReactDom from 'react-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
+
+import NoteContainer from './component/note-container';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes: []
+    }
+
+    this.getApp = this.getApp.bind(this);
+  }
+
+  componentDidUpdate() {
+    console.log('__STATE__', this.state);
+  }
+
+  getApp() {
+    return {
+      state: this.state,
+      setState: this.setState.bind(this)
+    }
+  }
+
+  render() {
+    return (
+      <main>
+        <BrowserRouter>
+          <section>
+            <Route exact path='/' component={() => <NoteContainer app={this.getApp()} />}
+          </section>
+        </BrowserRouter>
+      </main>
+    )
+  }
+}
+
+React.Dom.render(<App />, document.getElementById('root'))
